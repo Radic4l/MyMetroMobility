b54b6020df5cd3ec7e3b1bf209810640d1acdd52
Radic4l <axel.dujardin@Digital-Grenoble.local>
2018-08-24 17:18:25 +0200

    add project

---
+++ .cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class ConvertToJson
+{
+	public ConvertToJson()
+	{
+        List<Station> Stations = JsonConvert.DeserializeObject<List<Station>>(CallApi.ReceiveJson());
+
+        foreach (Station OneStation in Stations)
+        {
+            Console.WriteLine(OneStation.Name);
+            foreach (string Line in OneStation.Lines)
+            {
+                Console.WriteLine("\t" + Line);
+            }
+        }
+    }
+}
